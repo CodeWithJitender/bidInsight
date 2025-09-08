5eec7be8d991ab563658f465d481594733403a55
CodeWithJitender <jitender.developer786@gmail.com>
2025-09-08 12:11:47 +0530

    SignupModal

--- src/sections/profile/AiToolset.jsx
+++ src/sections/profile/AiToolset.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SignupModal from '../../components/SignupModal';
 
 function AiToolset() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className='h-screen w-full flex justify-center items-center'>
-      <img src="/coming-soon.jpg" className='rounded-lg max-w-xl' alt="" />
+      <img src="/coming-soon.jpg"   onClick={() => setIsModalOpen(true)} className='rounded-lg max-w-xl' alt="" />
+
+<SignupModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
   )
 }
