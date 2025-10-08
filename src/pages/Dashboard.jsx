a6f4fec646b3665258075761441224d9378411fc
CodeWithJitender <jitender.developer786@gmail.com>
2025-10-08 16:30:43 +0530

    responsive fixes v3

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -839,7 +839,7 @@ function Dashboard() {
                   />
                 )}
               </div>
-              <div className="feature-right md:w-full xl:w-auto">
+              <div className="feature-right md:w-full 2xl:w-auto ">
                 <div className="flex gap-4 items-center justify-between">
                   {/* Export Button (already has restrictions) */}
                   <div
@@ -871,7 +871,7 @@ function Dashboard() {
                     ) : exportLoading ? (
                       <div className="w-6 h-6 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
                     ) : (
-                      <img src="/export.png" className="w-6" alt="Export" />
+                      <img src="/export.png" className="w-6 min-w-6" alt="Export" />
                     )}
                   </div>
 
